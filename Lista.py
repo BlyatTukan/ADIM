b56abb7c465b16d2a4f7537567ebb7f35998a065
BlyatTukan <Overnex03@gmail.com>
2020-10-05 19:21:16 +0200

    Update Lista.py

--- Lista.py
+++ Lista.py
@@ -2,40 +2,42 @@ print("Witaj Użytkowniku")
 print("Wpisz Magiczne Słowo")
 MagicWord = str(input("Magiczne Słowo: ")) 
 MagicWord = list(MagicWord) 
-print("Czy chcesz bym zrobił kilka magicznych sztuczek z wpisanym słowem?") 
-print("TAK - wpisz  T")
-print("NIE - wpisz  N")
-Magic = input("Odpowiedź: ")
 
+while MagicWord != "":
+    print("Czy chcesz bym zrobił kilka magicznych sztuczek z wpisanym słowem?") 
+    print("TAK - wpisz  T")
+    print("NIE - wpisz  N")
+    Magic = input("Odpowiedź: ")
+    if Magic == "T":
+        print(MagicWord)
+        if "a" in MagicWord:
+            print("Twoje Słowo zawiera litery A")
+        elif "A" in MagicWord:
+            print("Twoje Słowo zawiera litery A")
+        else:
+            print("Twoje Słowo nie zawiera litery A")
+        if "e" in MagicWord:
+            print("Twoje Słowo zawiera litery e")
+        elif "E" in MagicWord:
+            print("Twoje Słowo zawiera litery E")
+        else:
+            print("Twoje Słowo nie zawiera litery E")
+        if "o" in MagicWord:
+            print("Twoje Słowo zawiera litery o")
+        elif "O" in MagicWord:
+            print("Twoje Słowo zawiera litery O")
+        else:
+            print("Twoje Słowo nie zawiera litery O")
+        print("Pierwsza litera twojego magicznego słowa to " + MagicWord[0])
+        print("Ostatnią literą twojego magicznego słowa to " + MagicWord[-1])
+        MagicWord.insert(0, "Napisałeś: ")
+        print(''.join(MagicWord))
+        break
+    elif Magic == "N":
+        print("Myśłałem że chcesz zobaczyć pare sztuczek")
+        break
+    else:
+        print("Podałeś złą odpowiedź jeszcze raz")
+        continue
 
-print(MagicWord)
-
-
-if "a" in MagicWord:
-    print("Twoje Słowo zawiera litery A")
-elif "A" in MagicWord:
-    print("Twoje Słowo zawiera litery A")
-else:
-    print("Twoje Słowo nie zawiera litery A")
-
-if "e" in MagicWord:
-    print("Twoje Słowo zawiera litery e")
-elif "E" in MagicWord:
-    print("Twoje Słowo zawiera litery E")
-else:
-    print("Twoje Słowo nie zawiera litery E")
-
-if "o" in MagicWord:
-    print("Twoje Słowo zawiera litery o")
-elif "O" in MagicWord:
-    print("Twoje Słowo zawiera litery O")
-else:
-    print("Twoje Słowo nie zawiera litery O")
-
-
-print("Pierwsza litera twojego magicznego słowa to " + MagicWord[0])
-print("Ostatnią literą twojego magicznego słowa to " + MagicWord[-1])
-
-
-MagicWord.insert(0, "Napisałeś: ")
-print(''.join(MagicWord)) 
+print("Do zobaczenia")
